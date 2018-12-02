fe5fb18bd353f0d65d4eac9e642b883626b1730f
hernannuss <hernannusshn@gmail.com>
2018-12-02 19:09:02 -0300

    color

--- assets/js/google-map-active.js
+++ assets/js/google-map-active.js
@@ -6,33 +6,21 @@
          $('.map')
       .gmap3({
         center:[-34.6029959, -58.392677],
-        zoom:11,
-        mapTypeId: "shadeOfGrey", // to select it directly
+        zoom:14,
+        mapTypeId: "mapa", // to select it directly
         mapTypeControlOptions: {
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, "shadeOfGrey"]
+          mapTypeIds: [google.maps.MapTypeId.ROADMAP, "mapa"]
         }
       })
         .marker({
             position: [-34.6029959,-58.392677 ],
           })
       .styledmaptype(
-        "shadeOfGrey",
+        "mapa",
         [
-          {"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},
-          {"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},
-          {"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-          {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},
-          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},
-          {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},
-          {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},
-          {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},
-          {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},
-          {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},
-          {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},
-          {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},
-          {"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}
+        
         ],
-        {name: "Shades of Grey"}
+        {name: ""}
       );
         
     });	  
